0f0b95a841392f3b8580ab12cf1cafb81da25325
Arcady Usov <itcrab@gmail.com>
2017-05-05 23:26:29 +0500

    Fix fixtures for "test_get_cache_path"

--- tests/cache_fixtures.py
+++ tests/cache_fixtures.py
@@ -38,26 +38,26 @@ def urls(tmpdir):
         'https://talkpython.fm/episodes/show/79/beeware-python-tools':
             os.path.join(
                 base_path,
-                '51c44bc4e085d30a948637068801a29c8f96037d9d9cac71adef189feae6e6d3.html',
+                'https___talkpython.fm_episodes_show_79_beeware-python-tools.html',
             ),
         'https://talkpython.fm/episodes/show/78/how-i-built-an-entire-game-and-toolchain-100-in-python':
             os.path.join(
                 base_path,
-                '63d4a34c6723ca999bb0dfbb7f2eefb113ed731d36ea928f086ba744608d67d9.html',
+                'https___talkpython.fm_episodes_show_78_how-i-built-an-entire-game-and-toolchain-100-in-python.html',
             ),
         'https://talkpython.fm/episodes/show/77/20-python-libraries-you-aren-t-using-but-should':
             os.path.join(
                 base_path,
-                'c179a06e2a5dd8eb025a0ebd0126bb8b54906329b80cc3b8fee0f568e6f0cc5f.html',
+                'https___talkpython.fm_episodes_show_77_20-python-libraries-you-aren-t-using-but-should.html',
             ),
         'https://talkpython.fm/episodes/show/76/renewable-python':
             os.path.join(
                 base_path,
-                '30053ae1f39d15092b94de51f03f6d2f35737e279b7ca718002e4569697ba9b4.html',
+                'https___talkpython.fm_episodes_show_76_renewable-python.html',
             ),
         'https://talkpython.fm/episodes/show/75/pythonic-games-at-checkio':
             os.path.join(
                 base_path,
-                '7609de695db3011fd20735a792d2f69bf7354e9d1d9bfd8185b9e2c9494dfeb5.html',
+                'https___talkpython.fm_episodes_show_75_pythonic-games-at-checkio.html',
             )
     }
