e20207a4b0a519fe20f2f1ce471601ff98416ee9
Arcady Usov <itcrab@gmail.com>
2017-05-15 00:35:00 +0500

    Drop don't needed exceptions

--- dark_keeper/exceptions.py
+++ dark_keeper/exceptions.py
@@ -1,6 +1,3 @@
-import math
-
-
 class DarkKeeperError(Exception):
     pass
 
@@ -27,35 +24,3 @@ class DarkKeeperRequestError(DarkKeeperError):
 
 class DarkKeeperRequestResponseError(DarkKeeperRequestError):
     pass
-
-
-class DarkKeeperStorageError(DarkKeeperError):
-    pass
-
-
-class DarkKeeperStorageExcelMaxLenStringError(DarkKeeperStorageError):
-    def __init__(self, length, max_length, xls_strmax_mul):
-        max_allowed = int(max_length / xls_strmax_mul)
-        new_xls_strmax_mul = math.ceil(
-            length / max_allowed
-        )
-
-        message = '''
-Error: length of parsed row string is more than
-max allowed ({max_allowed}) in Excel `{length} > {max_length}`
-
-Hint: you need change `xls_strmax_mul` when
-create `Storage` object
->>> ...
->>> xls_strmax_mul = {new_xls_strmax_mul}
->>> storage = Storage(model, export_dir, xls_strmax_mul)
-
-this changes raised error (warning) in Excel but file
-will be opened and you may see parsed data
-'''.format(
-            max_allowed=max_allowed,
-            length=length, max_length=max_length,
-            new_xls_strmax_mul=new_xls_strmax_mul
-        )
-
-        super().__init__(message)
