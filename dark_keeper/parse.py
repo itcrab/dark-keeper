ae2b6e3030db22867ff5b30b1d927ec36355c862
Arcady Usov <itcrab@gmail.com>
2017-07-08 18:03:44 +0500

    Drop don't used functions

--- dark_keeper/parse.py
+++ dark_keeper/parse.py
@@ -1,4 +1,3 @@
-from collections import OrderedDict
 from urllib.parse import urlparse, urljoin
 
 import lxml.html
@@ -45,42 +44,6 @@ def parse_attr(content, css_selector, css_attr):
         return
 
     return mp3_link[0].get(css_attr).strip()
-
-
-def create_new_data_row(soup, model):
-    row = OrderedDict([])
-    for field in model.items():
-        tags = soup.cssselect(field[1])
-
-        string = _tags_to_string(tags)
-        if len(string.strip(',').strip()):  # check blank
-            row.update({
-                field[0]: string
-            })
-
-    return row
-
-
-def _tags_to_string(tags):
-    data = ''
-    if len(tags) == 1:
-        data = _tag_to_string(tags[0])
-    elif len(tags) > 1:
-        _data = []
-        for tag in tags:
-            _data.append(_tag_to_string(tag))
-        data = ', '.join(_data)
-
-    return data
-
-
-def _tag_to_string(tag):
-    if tag.tag == 'a':
-        return tag.get('href')
-    elif tag.tag == 'audio':
-        return tag.get('src')
-
-    return tag.text_content().strip()
 
 
 def _calculate_start_url(base_url):
