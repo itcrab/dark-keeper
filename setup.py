4452081a8aa8ea229b1a152d43088677217ddfb5
Arcady Usov <itcrab@gmail.com>
2017-09-10 00:23:37 +0500

    Released version 0.2.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dark-keeper',
-    version='0.2.6',
+    version='0.2.7',
     packages=['dark_keeper'],
     url='https://github.com/itcrab/dark-keeper',
     license='MIT License',
