c2b3f3398640c080b32b231fdbf1fdaaedcd2d42
Arcady Usov <itcrab@gmail.com>
2020-06-03 00:10:08 +0500

    #5 Fix sample parsing for radio-t podcast site

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ class PodcastKeeper(DarkKeeper):
     mongo_uri = 'mongodb://localhost/podcasts/radio-t.com'
 
     def parse_urls(self, content):
-        urls = content.parse_urls('#blog-archives h1 a', self.base_url)
+        urls = content.parse_urls('.blog-archives .blog-archives-post .number-title a', self.base_url)
 
         return urls
 
     def parse_data(self, content):
         data = dict(
-            title=content.parse_text('.hentry .entry-title'),
-            desc=content.parse_text('.hentry .entry-content'),
-            mp3=content.parse_attr('.hentry audio', 'src'),
+            title=content.parse_text('.post-podcast .number-title'),
+            desc=content.parse_text('.post-podcast .post-podcast-content'),
+            mp3=content.parse_attr('.post-podcast .post-podcast-content audio', 'src'),
         )
 
         if data['title'] and data['mp3']:
